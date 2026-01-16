391704f0cc16174ae4f381ba0c6cbde231c8c565
Moritz Schepp <moritz.schepp@web.de>
2026-01-16 01:34:41 +0100

    feat: add chunk trimming method and refined System Prompt

--- backend/src/services/generation_service.py
+++ backend/src/services/generation_service.py
@@ -4,9 +4,26 @@ from .prompt.prompts_library import RAGPrompts
 
 class GenerationService:
     def __init__(self):
-        # TODO put config in a config file later.
         ollama_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
         self.llm_provider = OllamaProvider(model_name="llama3", base_url=ollama_url)
+
+    def _trim_chunks_to_fit_context(self, chunks: list[str], max_chars=20000)  -> list[str]:
+        # 20000 = 8000 tokens (llama3 context window 819) * 2,5 chars per token (4 chars per token usual estimate)
+        current_length = 0
+        selected_chunks = []
+
+        for chunk in chunks:
+            # +3 for chunk separation "\n---\n"
+            chunk_len = len(chunk) + 5
+
+            if current_length + chunk_len > max_chars:
+                print(f"DEBUG: Context limit reached via chunk trimming ({current_length}/{max_chars})")
+                break
+
+            selected_chunks.append(chunk)
+            current_length += chunk_len
+
+        return selected_chunks
 
     def generate_response_stream(self, query: str, retrieved_chunks: list):
         """
@@ -23,9 +40,9 @@ class GenerationService:
         # prepare the prompt
         system_instruction = RAGPrompts.SYSTEM_PROMPT_INSTRUCTED_GENERATION
 
-        formatted_prompt = RAGPrompts.format_main_prompt(query, clean_chunks)
+        final_chunks = self._trim_chunks_to_fit_context(clean_chunks)
 
-        print(f"DEBUG PROMPT:\n{formatted_prompt}\n--- END OF PROMPT ---\n")
+        formatted_prompt = RAGPrompts.format_main_prompt(query, final_chunks)
 
         # return the generator from the provider to stream the response
         return self.llm_provider.generate_stream(
